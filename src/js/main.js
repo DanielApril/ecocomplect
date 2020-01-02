0455e3d5b071a15d722f1d064978aba8f66920da
DanielApril <danielbfmv@yandex.ru>
2020-01-02 15:20:58 +0500

    add review slider

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,14 @@ $(document).ready(function () {
 
 	var firstScreenSlider = new Swiper('.first-screen-slider', {
 		loop: true,
-
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
-
 		pagination: {
 			el: '.swiper-pagination',
 			clickable: true
 		},
-
 		breakpoints: {
 			300: {
 				simulateTouch: true
@@ -29,10 +26,20 @@ $(document).ready(function () {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
-
 		slidesPerView: 'auto',
-
 		spaceBetween: 40
+
+	});
+
+
+	var reviewSlider = new Swiper('.reviews-slider', {
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		slidesPerView: 'auto',
+		centeredSlides: true,
+		spaceBetween: 32
 
 	});
 
